e0984530a4012ab38a628f4e2bb771e904a099ef
CourtneyWong <courtneyw0409@live.com>
2015-02-06 20:11:19 -0500

    begin writing playback

--- key.js
+++ key.js
@@ -8,24 +8,39 @@ sounds[49] = document.getElementById('fightsong_sound');
 images[49] = document.getElementById('fightsong_img');
 image_path[49] = 'images/prezbo.png';
 
-var history = new Array();
+var playHistory = [];
 
 function keyPress(e) {
-    play(e);
-    history.push(e.keyCode);
+	if(e.keyCode=='48'){
+    	playBack();
+	}
+    else{
+    	playKey(e);
+    	playHistory.push(e.keyCode);
+    	alert(playHistory);
+	}
 }
 
-function play(e)
+function playKey(e)
 {
 	sounds[e.keyCode].play();
     setTimeout(function(){
     	images[e.keyCode].src='';
-    },3500)
+    },sounds[e.keyCode].duration*1000)
     images[e.keyCode].src=image_path[e.keyCode];
 }
 
+function playAgain(code){
+	sounds[code].play();
+    setTimeout(function(){
+    	images[code].src='';
+    },sounds[code].duration*1000)
+    images[code].src=image_path[code];
+}
+
 function playBack() {
-	for (var i = 0; i < history.length; i++){
-		play(history[i]);
+	for (var i = 0; i < playHistory.length; i++){
+		playAgain(playHistory[i]);
 	}
 }
+
